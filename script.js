47277eac9ccdabebf36f01a088019ddfc6061f9d
Abhay Sharma <abhaykumarakela2004@gmail.com>
2026-04-26 20:28:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function toggleMenu() {
     nav.style.display = "flex";
   }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+let index = 0;
+
+const slides = document.getElementById("slides");
+const totalSlides = document.querySelectorAll(".slide").length;
+
+function showSlide() {
+    index++;
+
+    if (index >= totalSlides) {
+        index = 0;
+    }
+
+    slides.style.transform = "translateX(" + (-index * 100) + "%)";
+}
+
+setInterval(showSlide, 3000);
